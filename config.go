73488741266a13e5c917911739623130f164df91
ekonomizer <ekonomizer@mail.ru>
2018-10-11 13:42:34 +0300

    Update and rename goconfig.go to config.go

--- config.go
+++ config.go
@@ -1,4 +1,4 @@
-package goconfig
+package config
 
 import (
 	"reflect"
@@ -16,14 +16,14 @@ var (
 	validate *validator.Validate
 )
 
-// Load загружает конфигурацию из файла path в данную структуру config, используя
+// Load загружает конфигурацию из файла path в данную структуру cfg, используя
 // рефлексию. Если path не указан, или происходит ошибка, то возвращается конфиг
 // с дефолтными значениями.
-func Load(path string, config interface{}) error {
+func Load(path string, cfg interface{}) error {
 	viper.AutomaticEnv()
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	bindEnvironmentVars(config, "")
+	bindEnvironmentVars(cfg, "")
 
 	if path != "" {
 		viper.SetConfigFile(path)
@@ -33,11 +33,11 @@ func Load(path string, config interface{}) error {
 		return errors.Wrap(err, "unable to read config")
 	}
 
-	if err := viper.Unmarshal(config); err != nil {
+	if err := viper.Unmarshal(cfg); err != nil {
 		return errors.Wrap(err, "unable to parse config")
 	}
 
-	if err := validateConfig(config); err != nil {
+	if err := validateConfig(cfg); err != nil {
 		return errors.Wrap(err, "unable to validate config")
 	}
 
@@ -65,12 +65,12 @@ func getKey(ns, name string) string {
 	return res + name
 }
 
-func validateConfig(config interface{}) error {
+func validateConfig(cfg interface{}) error {
 	once.Do(func() {
 		validate = validator.New()
 	})
 
-	if err := validate.Struct(config); err != nil {
+	if err := validate.Struct(cfg); err != nil {
 		return err
 	}
 
